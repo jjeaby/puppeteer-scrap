f69943a55c67d267e0cddf762ff3f15f4bf77206
jjeaby <jjeaby@gmail.com>
2018-09-30 00:41:39 +0900

    라즈베라파이, x86 구분 코드 추가

---
+++ sample.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+var chromeBrowserPath =""
+const exec = require("child_process").exec
+exec("uname -a | grep 'armv'", (error, stdout, stderr) => {
+ //do whatever here
+        if( stdout != "") {
+                chromeBrowserPath = process.env.X86_CHROME_BROWSER_PATH;
+        } else {                 
+                chromeBrowserPath = process.env.RASPI_CHROME_BROWSER_PATH; 
+
+        }
+})
+
+ 
+
+const puppeteer = require('puppeteer');
+(async () => {
+        const browser = await puppeteer.launch({executablePath: chromeBrowserPath});
+        const page = await browser.newPage();
+        await page.goto('https://google.com');
+        await page.screenshot({path: 'google.png'});
+        await browser.close();
+})();
