cdab11f5fb42e30c8d7e89281059d33aa9b1dc38
lee yong jin <jjeaby@gmail.com>
2018-10-18 19:01:39 +0900

    LINUX_CHROME_BROWSER_PATH 변수 변경

--- newspaper/newspaper_etnews.js
+++ newspaper/newspaper_etnews.js
@@ -10,9 +10,7 @@ const uname = execSync('uname -a');
 let chromeBrowserPath;
 let headlessFlag = false;
 
-const etnewsUrl = process.env.ETNEWS_03_URL;
-let screenShotName = etnewsUrl.replace("https://", "").replace("http://", "").replace(/\//g, ".",);
-console.log(screenShotName);
+
 
 const VIEWPORT = {width: 1280, height: 1024, deviceScaleFactor: 2};
 
@@ -43,56 +41,68 @@ else {
             await page.setViewport(VIEWPORT);
         }
         await page.tracing.start({path: 'trace.json', categories: ['devtools.timeline']})
-        await page.goto(etnewsUrl);
 
-        // util.writeFile('output_dongait.txt', '', 'w');
+        etnewsUrlList = [ process.env.ETNEWS_02_URL, process.env.ETNEWS_03_URL ];
 
-        //page.on('console', util.logRequest);
-        await util.wait(page, '//div[@class=\'list_wrap\']/ul[@class=\'list_news\']/li[1]//a');
-        await page.screenshot({path: screenShotName + '-start.png'});
-        try {
-            // for (let pageNum = 1; pageNum <= 1684; pageNum++) {
-            for (let pageNum = 1; pageNum <= 94; pageNum++) {
-                await page.goto('http://www.etnews.com/news/section.html?id1=03&page=' + pageNum)
-                await page.goto(etnewsUrl + '&page=' + pageNum);
+        etnewsUrlList.forEach( async function(URL) {
 
-                let date = await util.getText(page, '//div[@class=\'list_wrap\']/ul[@class=\'list_news\']/li[1]//dd[@class=\'date\']/span[2]');
-                if (date.toString().split(" ")[0] !== util.getYesterdayDate()) {
-                    break;
-                }
 
-                for (let index = 1; index <= 15; index++) {
+            let screenShotName = URL.replace("https://", "").replace("http://", "").replace(/\//g, ".",);
+            console.log(screenShotName);
 
-                    if (index < 5 || index > 9) {
+            await page.goto(URL);
 
-                        console.log(pageNum + "-" + index);
+            // util.writeFile('output_dongait.txt', '', 'w');
 
-                        let date = await util.getText(page, '//div[@class=\'list_wrap\']/ul[@class=\'list_news\']/li[' + index + ']//dd[@class=\'date\']/span[2]');
-                        if (date.toString().split(" ")[0] !== util.getYesterdayDate()) {
-                            break;
-                        }
-                        // await util.click(page, '//ul[@class=\'list_news\']/li[' + index + ']/dl//a');
-                        await page.click('ul.list_news > li:nth-of-type(' + index + ') > dl a')
-                        await util.wait(page, '//article/section[@id=\'articleBody\']/p');
+            //page.on('console', util.logRequest);
+            await util.wait(page, '//div[@class=\'list_wrap\']/ul[@class=\'list_news\']/li[1]//a');
+            await page.screenshot({path: screenShotName + '-start.png'});
+            try {
+                // for (let pageNum = 1; pageNum <= 1684; pageNum++) {
+                for (let pageNum = 1; pageNum <= 94; pageNum++) {
+                    await page.goto('http://www.etnews.com/news/section.html?id1=03&page=' + pageNum)
+                    await page.goto(etnewsUrl + '&page=' + pageNum);
 
-                        let articletText = await util.getText(page, '//article/section[@id=\'articleBody\']/p');
-                        util.writeFile('etnews_output.txt', articletText, 'a');
-                        //console.log(date + ' ' + articletText)
-                        await page.goBack();
+                    let date = await util.getText(page, '//div[@class=\'list_wrap\']/ul[@class=\'list_news\']/li[1]//dd[@class=\'date\']/span[2]');
+                    if (date.toString().split(" ")[0] !== util.getYesterdayDate()) {
+                        break;
                     }
+
+                    for (let index = 1; index <= 15; index++) {
+
+                        if (index < 5 || index > 9) {
+
+                            console.log(pageNum + "-" + index);
+
+                            let date = await util.getText(page, '//div[@class=\'list_wrap\']/ul[@class=\'list_news\']/li[' + index + ']//dd[@class=\'date\']/span[2]');
+                            if (date.toString().split(" ")[0] !== util.getYesterdayDate()) {
+                                break;
+                            }
+                            // await util.click(page, '//ul[@class=\'list_news\']/li[' + index + ']/dl//a');
+                            await page.click('ul.list_news > li:nth-of-type(' + index + ') > dl a')
+                            await util.wait(page, '//article/section[@id=\'articleBody\']/p');
+
+                            let articletText = await util.getText(page, '//article/section[@id=\'articleBody\']/p');
+                            util.writeFile('etnews_output.txt', articletText, 'a');
+                            //console.log(date + ' ' + articletText)
+                            await page.goBack();
+                        }
+                    }
+                    //await util.click(page, '//li[@class=\'next\']/a')
+
+
                 }
-                //await util.click(page, '//li[@class=\'next\']/a')
 
-
+                await util.sleep(5000);
+                await page.screenshot({path: screenShotName + '-end.png'});
+                await browser.close();
+            } catch (e) {
+                console.log(e);
+                await page.screenshot({path: screenShotName + '-end.png'});
+                await browser.close();
             }
+        })
 
-            await util.sleep(5000);
-            await page.screenshot({path: screenShotName + '-end.png'});
-            await browser.close();
-        } catch (e) {
-            console.log(e);
-            await page.screenshot({path: screenShotName + '-end.png'});
-            await browser.close();
-        }
+
     }
 )();
