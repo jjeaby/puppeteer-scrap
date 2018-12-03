751dc3eb00c17684079dcc81c0c7b03d915a76e0
jjeaby <jjeaby@gmail.com>
2018-12-04 00:16:00 +0900

    modift config

--- clien/clien_scarp.js
+++ clien/clien_scarp.js
@@ -29,91 +29,84 @@ else {
 
 (async () => {
 
-        clienUrlList = [process.env.CLIEN_PARK];
-
-        // etnewsUrlList.forEach( async function(URL) {
-        for (let i = 0; i < clienUrlList.length; i++) {
-            const browser = await puppeteer.launch({
-                executablePath: chromeBrowserPath,
-                headless: headlessFlag,
-                args: ["--disable-notifications", "--disable-gpu", "--disable-glsl-translator", "--disable-translate-new-ux"],
-                networkIdleTimeout: 5000,
-                timeout: 3000000
-            });
-
-            const page = await browser.newPage();
-            if (VIEWPORT) {
-                await page.setViewport(VIEWPORT);
-            }
-            await page.tracing.start({path: 'trace.json', categories: ['devtools.timeline']})
+        URL = process.env.CLIEN_PARK;
 
 
-            let URL = clienUrlList[i];
-            let screenShotName = URL.replace("https://", "").replace("http://", "").replace(/\//g, ".",);
-            console.log(screenShotName);
 
-            await page.goto(URL);
+        try {
+            // for (let pageNum = 1; pageNum <= 1684; pageNum++) {
+            for (let pageNum = 0; pageNum <= 99999999999999; pageNum++) {
+                const browser = await puppeteer.launch({
+                    executablePath: chromeBrowserPath,
+                    headless: headlessFlag,
+                    args: ["--disable-notifications", "--disable-gpu", "--disable-glsl-translator", "--disable-translate-new-ux"],
+                    networkIdleTimeout: 25000,
+                    timeout: 3000000
+                });
 
-            // util.writeFile('output_dongait.txt', '', 'w');
+                const page = await browser.newPage();
+                if (VIEWPORT) {
+                    await page.setViewport(VIEWPORT);
+                }
+                await page.tracing.start({path: 'trace.json', categories: ['devtools.timeline']})
 
-            //page.on('console', util.logRequest);
-            await util.wait(page, '//div[@class=\'board_head\']/div[@class=\'board_name\']/h2/a');
-            await page.screenshot({path: screenShotName + '-start.png'});
-            try {
-                // for (let pageNum = 1; pageNum <= 1684; pageNum++) {
-                for (let pageNum = 0; pageNum <= 99999999999999; pageNum++) {
-                    await page.goto(URL + '&po=' + pageNum);
 
-                    // let date = await util.getText(page, '#div_content > div:nth-child(8) > div.list_time > span > span');
+                let screenShotName = URL.replace("https://", "").replace("http://", "").replace(/\//g, ".",);
+                console.log(screenShotName);
+
+                await page.goto(URL);
+                await util.wait(page, '//div[@class=\'board_head\']/div[@class=\'board_name\']/h2/a');
+                await page.screenshot({path: screenShotName + '-start.png'});
+
+
+                await page.goto(URL + '&po=' + pageNum);
+
+                // let date = await util.getText(page, '#div_content > div:nth-child(8) > div.list_time > span > span');
+                let date = await util.getText(page, '//*[@id="div_content"]/div[7]/div[5]/span/span');
+                date = date.split(' ');
+                console.log(date[0] + ' ' + util.getYesterdayDate().replace(/\./g, '-'));
+                // if (date[0] !== util.getYesterdayDate().replace(/\./g,'-')) {
+                //     break;
+                // }
+                if (date[0] === '2017-12-31') {
+                    break;
+                }
+
+
+                for (let index = 1; index <= 30; index++) {
+                    console.log(pageNum + "-" + index);
+
                     let date = await util.getText(page, '//*[@id="div_content"]/div[7]/div[5]/span/span');
                     date = date.split(' ');
-                    console.log(date[0] + ' ' + util.getYesterdayDate().replace(/\./g,'-'));
+                    console.log(date[0] + ' ' + util.getYesterdayDate().replace(/\./g, '-'));
                     // if (date[0] !== util.getYesterdayDate().replace(/\./g,'-')) {
                     //     break;
                     // }
                     if (date[0] === '2017-12-31') {
-                         break;
+                        break;
                     }
 
 
-                    for (let index = 1; index <= 30; index++) {
-                        console.log(pageNum + "-" + index);
-
-                        let date = await util.getText(page, '//*[@id="div_content"]/div[7]/div[5]/span/span');
-                        date = date.split(' ');
-                        console.log(date[0] + ' ' + util.getYesterdayDate().replace(/\./g,'-'));
-                        // if (date[0] !== util.getYesterdayDate().replace(/\./g,'-')) {
-                        //     break;
-                        // }
-                        if (date[0] === '2017-12-31') {
-                            break;
-                        }
-
-
-
-                        let selectLine = index + 6;
-                        try {
-                            await page.click('#div_content > div:nth-child(' + selectLine + ') > div.list_title > a > span');
-                        } catch (e) {
-                            continue;
-                        }
-                        await util.sleep(1000);
-                        await util.wait(page, '//*[@id="div_content"]/div[4]/div[2]/article/div');
-
-                        let articletText = await util.getText(page, '//*[@id="div_content"]/div[4]/div[2]/article/div');
-
-                        let saveText = util.removeUnnecessaryChar(articletText);
-                        saveText.forEach( function(saveText) {
-                            if( saveText.trim().length > 0 ) {
-                                util.writeFile('clien_' + date[0] + '.txt', saveText, 'a');
-                            }
-                        });
-                        //console.log(date + ' ' + articletText)
-                        // await page.goBack();
-                        await page.goto(URL + '&po=' + pageNum);
-
-
+                    let selectLine = index + 6;
+                    try {
+                        await page.click('#div_content > div:nth-child(' + selectLine + ') > div.list_title > a > span');
+                    } catch (e) {
+                        continue;
                     }
+                    await util.sleep(1000);
+                    await util.wait(page, '//*[@id="div_content"]/div[4]/div[2]/article/div');
+
+                    let articletText = await util.getText(page, '//*[@id="div_content"]/div[4]/div[2]/article/div');
+
+                    let saveText = util.removeUnnecessaryChar(articletText);
+                    saveText.forEach(function (saveText) {
+                        if (saveText.trim().length > 0) {
+                            util.writeFile('clien_' + date[0] + '.txt', saveText, 'a');
+                        }
+                    });
+                    //console.log(date + ' ' + articletText)
+                    // await page.goBack();
+                    await page.goto(URL + '&po=' + pageNum);
 
 
                 }
@@ -121,13 +114,15 @@ else {
                 await util.sleep(5000);
                 await page.screenshot({path: screenShotName + '-end.png'});
                 await browser.close();
-            } catch (e) {
-                console.log(e);
-                await page.screenshot({path: screenShotName + '-end.png'});
-                await browser.close();
+
             }
+        } catch (e) {
+            console.log(e);
+            await page.screenshot({path: screenShotName + '-end.png'});
+            await browser.close();
         }
 
-
     }
+
+
 )();
